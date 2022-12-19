da7478b0217649b3497812cf2c65006ce62c7988
sung <gulbi79@gmail.com>
2022-12-19 16:52:32 +0900

    thymeleaf fragments 작업중

--- src/main/java/wi/com/wisnop/controller/th/AuthController.java
+++ src/main/java/wi/com/wisnop/controller/th/AuthController.java
@@ -1,4 +1,4 @@
-package wi.com.wisnop.controller.common;
+package wi.com.wisnop.controller.th;
 
 import java.util.HashMap;
 import java.util.List;
@@ -33,7 +33,7 @@ import wi.com.wisnop.service.common.CommonService;
 import wi.com.wisnop.service.common.LoginService;
 
 @Controller
-@RequestMapping(value = "/auth")
+@RequestMapping(value = "/th/auth")
 @RequiredArgsConstructor
 //@Slf4j
 public class AuthController {
@@ -125,7 +125,7 @@ public class AuthController {
     		//비밀번호 일치
 			if ("Y".equals(userVo.getPwCompareYn())) {
 				setSessionInit(request, userVo, mav, paramMap);
-				mav.setViewName("redirect:/auth/goPortal");
+				mav.setViewName("redirect:/th/auth/goPortal");
 
 			} else if (StringUtils.isEmpty(userVo.getPwd())) {
 				rtnMap.put("errCode", -3);
@@ -178,103 +178,4 @@ public class AuthController {
   		SessionUtil.setAttribute(Namespace.BU_LIST, userBuList);
   	}
     
-    /*
-
-    @GetMapping("/auth/logout")
-    public String logoutView() {
-    	return "auth/logout";
-    }
-
-    @GetMapping("/auth/signup")
-    public String signupView() {
-        return "auth/signup";
-    }
-
-    @PostMapping("/auth/signupProc")
-    public String signup(UserVo userVo) {
-        userService.joinUser(userVo);
-        return "redirect:/auth/login";
-    }
-    
-    @GetMapping("/home")
-    public String loginAfterView(@AuthenticationPrincipal UserVo uservo, Model model) {
-    	//1.로그인한 유저의 권한에 따른 메뉴 조회
-    	HashMap<String,Object> paramMap = new HashMap<String,Object>();
-    	paramMap.put("userId", uservo.getUserId());
-    	model.addAttribute("menuList", userService.userMenu(paramMap));
-    	
-        return "layouts/top";
-    }
-
-    @GetMapping("/auth/denied")
-    public String deniedView() {
-        return "auth/denied";
-    }
-
-    @GetMapping("/auth/sessionexpired")
-    public String sessionexpiredView() {
-    	return "auth/sessionexpired";
-    }
-    
-    @SuppressWarnings("unchecked")
-	@GetMapping("/page/{menuCd}")
-    public String goToInnerView(@PathVariable String menuCd, Model model, @AuthenticationPrincipal UserVo uservo) {
-    	String pageUrl = "auth/denied";
-    	
-    	if (uservo == null || StringUtils.isEmpty(uservo.getUserId())) return "auth/sessionexpired";
-    	
-    	//화면에서 받은 menu code로 mapping url을 조회 후 해당 화면을 리턴
-    	HashMap<String,Object> paramMap = new HashMap<String,Object>();
-    	paramMap.put("userId", uservo.getUserId());
-    	paramMap.put("menuCd", menuCd);
-    	List<HashMap<String, String>> menuList = userService.userMenu(paramMap);
-    	
-    	if (menuList.size() > 0) {
-    		HashMap<String, String> map = menuList.get(0);
-    		String url = map.get("url");
-    		String tree = map.get("tree");
-    		if (url != null && !url.isEmpty()) {
-    			pageUrl = url;
-    			
-    			//tree 조회
-    			HashMap<String, Object> treeMap = commonService.selectTree();
-    			model.addAllAttributes(treeMap); //salesTree, productTree
-
-    			//ui에서 사용하기 편하게 레벨별로 분기한다.
-    			
-    			//sales -----------------------
-    			if (tree.indexOf(",SALES,") != -1) {
-    				List<HashMap<String, String>> salesList = (List<HashMap<String, String>>)treeMap.get("salesTree");
-    				model.addAttribute("sales1",salesList.stream().filter(t->"1".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //salesTree 1 level
-    				model.addAttribute("sales2",salesList.stream().filter(t->"2".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //salesTree 2 level
-    				model.addAttribute("sales3",salesList.stream().filter(t->"3".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //salesTree 3 level
-    				model.addAttribute("sales4",salesList.stream().filter(t->"4".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //salesTree 4 level
-    				model.addAttribute("sales5",salesList.stream().filter(t->"5".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //salesTree 5 level
-    				
-				//product -----------------------
-    			} else if (tree.indexOf(",PRODUCT,") != -1) {
-    				List<HashMap<String, String>> productList = (List<HashMap<String, String>>)treeMap.get("productTree");
-    				
-    				model.addAttribute("prod1",productList.stream().filter(t->"1".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //productTree 1 level
-    				model.addAttribute("prod2",productList.stream().filter(t->"2".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //productTree 2 level
-    				model.addAttribute("prod3",productList.stream().filter(t->"3".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //productTree 3 level
-    				model.addAttribute("prod4",productList.stream().filter(t->"4".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //productTree 4 level
-    				model.addAttribute("prod5",productList.stream().filter(t->"5".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //productTree 5 level
-
-    			//plant -----------------------
-    			} else if (tree.indexOf(",PLANT,") != -1) {
-    				List<HashMap<String, String>> plantList = (List<HashMap<String, String>>)treeMap.get("plantTree");
-    				
-    				model.addAttribute("plant1",plantList.stream().filter(t->"1".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //plantTree 1 level
-    				model.addAttribute("plant2",plantList.stream().filter(t->"2".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //plantTree 2 level
-    				model.addAttribute("plant3",plantList.stream().filter(t->"3".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //plantTree 3 level
-    				model.addAttribute("plant4",plantList.stream().filter(t->"4".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //plantTree 4 level
-    				model.addAttribute("plant5",plantList.stream().filter(t->"5".equals(String.valueOf(t.get("levelCd")))).collect(Collectors.toList())); //plantTree 5 level
-    			}
-    		}
-    		model.addAllAttributes(map);
-    	}
-        return pageUrl;
-    }
-    */
 }
